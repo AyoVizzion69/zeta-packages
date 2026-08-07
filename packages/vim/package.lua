91c14ad9dbf8abac67f969c277da1a60fc41dc95
throwaway-account-58577 <throwaway_account_58577@yahoo.com>
2026-08-07 06:59:18 +0100

    remove test from package

--- packages/vim/package.lua
+++ packages/vim/package.lua
@@ -6,7 +6,4 @@ return {
   sha256  = "bf6f93b96d4c64fcc0eca236548ab95532b63b1624efcbc265608c6df4bd3458",
   deps    = {},
   archive = { strip = 1 },
-  test    = function(p)
-    p:run("make test")
-  end,
 }
