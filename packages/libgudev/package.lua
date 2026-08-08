7e6da0aea2a1e522d3db7fbc8370a2b04e85ad9d
gretagen <besternet91@gmail.com>
2026-08-08 10:02:38 +0200

    Add libinput 1.31.3 with libevdev/libmtdev/libgudev/libwacom deps

---
+++ packages/libgudev/package.lua
@@ -0,0 +1,12 @@
+return {
+  name    = "libgudev",
+  version = "238",
+  summary = "GObject bindings for libudev",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/libgudev/libgudev-238.tar.gz",
+  sha256  = "696366061dbf99a679de1453ac5ee41a0e93add105527c63d681eece0dd88434",
+  deps    = {},
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -e " .. p.install_root .. "/usr/lib/libgudev-1.0.so.0")
+  end,
+}
---
+++ packages/libinput/package.lua
@@ -0,0 +1,12 @@
+return {
+  name    = "libinput",
+  version = "1.31.3",
+  summary = "Input device and event processing library",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/libinput/libinput-1.31.3.tar.gz",
+  sha256  = "8c8deaad05c5490f02a464f68354d4b73af212b624e86ad7b18713e5ce715aa0",
+  deps    = { "libevdev", "libmtdev", "libwacom" },
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -e " .. p.install_root .. "/usr/lib/libinput.so.10")
+  end,
+}
