04c7ca31b969f38f80705bd9d7a1ffebf8a80f17
gretagen <besternet91@gmail.com>
2026-08-08 09:04:38 +0200

    Add doas 6.8.2 (PAM, setuid root) with linux-pam 1.7.2 dep

---
+++ packages/doas/package.lua
@@ -0,0 +1,12 @@
+return {
+  name    = "doas",
+  version = "6.8.2",
+  summary = "Execute commands as another user (portable OpenBSD doas)",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/doas/doas-6.8.2.tar.gz",
+  sha256  = "d7393618ed73021d3ad5e86cb742d73348ebf357fd87245edad221a248b5f27c",
+  deps    = { "pam" },
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -x " .. p.install_root .. "/usr/bin/doas")
+  end,
+}
