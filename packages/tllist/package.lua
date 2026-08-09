1236f7cb141059c71ef97456cb9463e04191e3d7
= <=>
2026-08-09 13:16:37 +0200

    add desc

--- packages/tllist/package.lua
+++ packages/tllist/package.lua
@@ -1,7 +1,7 @@
 return {
   name    = "tllist",
   version = "1.1.0",
-  summary = "Short description",
+  summary = "Typed Linked List C header library",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/tllist/tllist-1.1.0.tar.gz",
   sha256  = "76b2a6b12ecb304ffbfbffbe1fbc16ba82452bf56058947e90afcfa9cdf4a8a6",
   deps    = {},
