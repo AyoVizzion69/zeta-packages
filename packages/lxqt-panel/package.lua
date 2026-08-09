2c0dcd8d49b8d34bd47fd911a095227ac70a1a9a
gretagen <besternet91@gmail.com>
2026-08-09 07:14:11 +0200

    Rebuild lxqt-panel from full scratch env (static plugins verified: 10 plugins link into binary by design; all 18 plugin .desktop files shipped)

--- packages/lxqt-panel/package.lua
+++ packages/lxqt-panel/package.lua
@@ -3,7 +3,7 @@ return {
   version = "2.1.0",
   summary = "LXQt component (lxqt-panel)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/lxqt-panel/lxqt-panel-2.1.0.tar.gz",
-  sha256  = "46aa9d7782f4e1745e8e008c6656928d39e61576754c9ed897d64005cd65b854",
+  sha256  = "07e16c48bb7832e51e15ac8e7ebaeb5e35e9d9d454406ee6add0bf6605ac64b0",
   deps    = { "kwindowsystem", "layer-shell-qt", "libXtst", "libdbusmenu-lxqt", "liblxqt", "libqtxdg", "lxqt-globalkeys", "qtbase", "qtsvg", "qtwayland", "solid", "wayland", "xcb-util", "xcb-util-image", "xkbcommon" },
   archive = { strip = 1 },
   test    = function(p)
