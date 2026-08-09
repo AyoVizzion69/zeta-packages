5375a1fbb70d24a29e5a645cd948427625cf555e
gretagen <besternet91@gmail.com>
2026-08-09 05:18:35 +0200

    Declare full runtime dep closure for LXQt stack (all repo-provided NEEDED libs; excludes dbus/polkit/glib core)

--- packages/lxqt-panel/package.lua
+++ packages/lxqt-panel/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "LXQt component (lxqt-panel)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/lxqt-panel/lxqt-panel-2.1.0.tar.gz",
   sha256  = "46aa9d7782f4e1745e8e008c6656928d39e61576754c9ed897d64005cd65b854",
-  deps    = { "qtbase", "qtsvg", "qtwayland", "libqtxdg", "kwindowsystem", "layer-shell-qt", "lxqt-globalkeys", "libdbusmenu-lxqt", "solid", "liblxqt", "libXtst" },
+  deps    = { "kwindowsystem", "layer-shell-qt", "libXtst", "libdbusmenu-lxqt", "liblxqt", "libqtxdg", "lxqt-globalkeys", "qtbase", "qtsvg", "qtwayland", "solid", "wayland", "xcb-util", "xcb-util-image", "xkbcommon" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/lxqt-panel")
--- packages/qtbase/package.lua
+++ packages/qtbase/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Qt 6 base libraries",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/qtbase/qtbase-6.8.2.tar.gz",
   sha256  = "cfa14cd9b84a9ce03368f6a5559c1ea67dd1a181e35fe292e0bfc2040134d7ad",
-  deps    = { "libICE", "libSM" },
+  deps    = { "at-spi2-core", "cairo", "double-conversion", "fontconfig", "freetype", "gdk-pixbuf", "gtk3", "harfbuzz", "libICE", "libSM", "libb2", "libdrm", "libinput", "libmtdev", "libpng", "libz", "md4c", "pango", "pcre2", "xcb-util-cursor", "xcb-util-image", "xcb-util-keysyms", "xcb-util-renderutil", "xcb-util-wm", "xkbcommon", "zstd" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -e " .. p.install_root .. "/usr/lib/libQt6Core.so.6 -a -e " .. p.install_root .. "/usr/lib/libQt6Gui.so.6")
