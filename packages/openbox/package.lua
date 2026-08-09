3a61a3687aaac025df167125123d56106a4271b4
gretagen <besternet91@gmail.com>
2026-08-09 09:17:29 +0200

    Ship openbox Clearlooks theme (rc.xml default) so openbox doesn't exit 1 on startup

--- packages/openbox/package.lua
+++ packages/openbox/package.lua
@@ -3,7 +3,7 @@ return {
   version = "3.6.1",
   summary = "Openbox X11 window manager (LXQt default WM)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/openbox/openbox-3.6.1.tar.gz",
-  sha256  = "cb5aedb886fa02a483793bed780eb6ed04f5010baf723d2a918b2130edca53e3",
+  sha256  = "399bb121acafaf59f88a98b952855f1e2842920a70f90a80200a182c5421c150",
   deps    = { "libXcursor", "libXinerama", "libXrandr", "libXext", "libICE", "libSM", "pango", "libXft", "librsvg", "cairo", "libxml2", "imlib2" },
   archive = { strip = 1 },
   test    = function(p)
