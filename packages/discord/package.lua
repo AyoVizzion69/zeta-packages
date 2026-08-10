29621c15e48456d2b2845a8df62d1e64b3c6c827
gretagen <gretagen@local>
2026-08-10 02:12:29 +0200

    discord: rebuild with module-seed wrapper launcher (sha 5d5c9b3d)

--- packages/discord/package.lua
+++ packages/discord/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.0.152",
   summary = "Discord (stable Electron client)",
   url     = "https://media.githubusercontent.com/media/gretagen/zeta-artifacts/refs/heads/main/packages/discord/discord-1.0.152.tar.gz",
-  sha256  = "41755037cfc2b49fb80baaa74e21b06791590a3d053981c4743140c779b914ff",
+  sha256  = "5d5c9b3dbc83500a9bf76723b400fec8b36316ae8b31379829d0d0296e9776e4",
   deps    = { "alsa-lib", "at-spi2-core", "cairo", "dbus", "expat", "glib", "gtk3", "libX11", "libXcomposite", "libXcursor", "libXdamage", "libXext", "libXfixes", "libXi", "libXrandr", "libXrender", "libxss", "libXtst", "libdrm", "libxcb", "mesa", "nss", "pango", "pulseaudio", "wayland", "xkbcommon" },
   archive = { strip = 1 },
   test    = function(p)
