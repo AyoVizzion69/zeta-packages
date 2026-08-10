5c9e18c6d32adee9b60b5372db4e9109c0689ea4
gretagen <gretagen@local>
2026-08-10 08:50:04 +0200

    drop dbus and pam packages (already provided by base system)

--- packages/doas/package.lua
+++ packages/doas/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Execute commands as another user (portable OpenBSD doas)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/doas/doas-6.8.2.tar.gz",
   sha256  = "d7393618ed73021d3ad5e86cb742d73348ebf357fd87245edad221a248b5f27c",
-  deps    = { "pam" },
+  deps    = {},
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/doas")
--- packages/polkit/package.lua
+++ packages/polkit/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Authorization framework for desktop privileges",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/polkit/polkit-127.tar.gz",
   sha256  = "576b97ed97e438365b51920d6e5480c13ae6a3e26ea9bc93bea4b298d0d4aa3f",
-  deps    = { "expat", "glib", "pam" },
+  deps    = { "expat", "glib" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -e " .. p.install_root .. "/usr/lib/libpolkit-gobject-1.so.0")
