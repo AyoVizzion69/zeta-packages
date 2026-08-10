f2cede5d83855a8f23f8bf959a2124e55c2b1194
gretagen <gretagen@local>
2026-08-10 02:27:15 +0200

    flatpak: fix dep name json-glib -> libjson-glib (zeta resolves deps by exact repo dir name)

--- packages/flatpak/package.lua
+++ packages/flatpak/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Linux application sandboxing and distribution framework",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/flatpak/flatpak-1.18.0.tar.gz",
   sha256  = "725472908332cf727dc679e35924179a3d8ee4f3250fcfdb433853fda39c20fe",
-  deps    = { "libostree", "libdconf", "libappstream", "libcurl", "libxml2", "libarchive", "libseccomp", "libfuse3", "json-glib", "gpgme" },
+  deps    = { "libostree", "libdconf", "libappstream", "libcurl", "libxml2", "libarchive", "libseccomp", "libfuse3", "libjson-glib", "gpgme" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/flatpak")
