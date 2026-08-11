4361e0217934fcd1bde2ebb0ea928fb15dc7536e
gretagen <besternet91@gmail.com>
2026-08-11 19:35:25 +0200

    wmaker: link libjpeg.so.62 (repo libjpeg-turbo 3.2 ABI) instead of host libjpeg.so.8

--- packages/wmaker/package.lua
+++ packages/wmaker/package.lua
@@ -3,7 +3,7 @@ return {
   version = "0.96.0",
   summary = "Window Maker: GNUstep-compliant NeXTstep window manager clone",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/wmaker/wmaker-0.96.0.tar.gz",
-  sha256  = "35c9309fbba2c8c79c110092b9daefdc433a02996fe62b2b0d5d938332430b11",
+  sha256  = "5b82697ec6adf946bd9c5c2f7f23f4fd116632c48b427e65e83b543fca1f5625",
   deps    = { "libX11", "libXext", "libXinerama", "libXres", "libXmu", "libXpm", "libXft", "libpng", "libjpeg-turbo", "fontconfig", "pango" },
   archive = { strip = 1 },
   test    = function(p)
