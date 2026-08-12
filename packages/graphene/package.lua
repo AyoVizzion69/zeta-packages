a07eb325c9f131987908d91a8e15698254149ed7
gretagen <besternet91@gmail.com>
2026-08-12 11:46:14 +0200

    graphene/gtk4/gtkmm4: package 1.10.8/4.14.5/4.14.0 (gtkmm4 stack: gtk4 X11+wayland, no media/vulkan; gtkmm4 built against staged glibmm-2.68/cairomm-1.16/pangomm-2.48)

---
+++ packages/graphene/package.lua
@@ -0,0 +1,12 @@
+return {
+  name    = "graphene",
+  version = "1.10.8",
+  summary = "Thin layer of math types for graphic libraries (required by GTK4)",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/graphene/graphene-1.10.8.tar.gz",
+  sha256  = "31de078506859efbc36ddc97b201f1c42a4b29608aca3b12c615cd72c3a35864",
+  deps    = { "glib" },
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -f " .. p.install_root .. "/usr/lib/libgraphene-1.0.so.0")
+  end,
+}
