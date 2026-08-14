b0dbf2e8ff322b7ee4672728c67e4305a477833d
gretagen <besternet91@gmail.com>
2026-08-14 22:06:53 +0200

    flatpak: remove dbus .service files that block zeta install
    The .service suffix triggers zeta's init-system blocklist. These files
    enable automatic dbus activation of flatpak services which is optional.

--- packages/flatpak/package.lua
+++ packages/flatpak/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.18.0",
   summary = "Linux application sandboxing and distribution framework",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/flatpak/flatpak-1.18.0.tar.gz",
-  sha256  = "3248a90f3b217b0053805175d6c879e116c536bee41d202cb641ec4d83cc58e7",
+  sha256  = "583fd4db00e88c2216fe3e86d34398413c33327378dc08f1ec340c32db28e7e0",
   deps    = { "libostree", "libdconf", "libappstream", "libcurl", "libxml2", "libarchive", "libseccomp", "libfuse3", "libjson-glib", "gpgme" },
   archive = { strip = 1 },
   test    = function(p)
