b8e905b50a94ba4824bb45fd3e019e5c55b4c78c
gretagen <besternet91@gmail.com>
2026-08-14 20:32:15 +0200

    doas: fix PAM config to use pam_unix.so
    Replace include system-auth with required pam_unix.so so authentication
    works on Zerene OS.

--- packages/doas/package.lua
+++ packages/doas/package.lua
@@ -3,10 +3,11 @@ return {
   version = "6.8.2",
   summary = "Execute commands as another user (portable OpenBSD doas)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/doas/doas-6.8.2.tar.gz",
-  sha256  = "d7393618ed73021d3ad5e86cb742d73348ebf357fd87245edad221a248b5f27c",
+  sha256  = "6313a5e26ec179d7c527bbfbcc209c519e707cc2731aef8f5f3343dbac361051",
   deps    = {},
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/doas")
+    p:run("grep -q 'pam_unix.so' " .. p.install_root .. "/etc/pam.d/doas")
   end,
 }
