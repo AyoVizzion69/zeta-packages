3826651986184f55641d3b37aff9406dca52fcd5
gretagen <besternet91@gmail.com>
2026-08-15 07:55:25 +0200

    libostree: remove absolute grub.d symlink that broke archive validation

--- packages/libostree/package.lua
+++ packages/libostree/package.lua
@@ -3,7 +3,7 @@ return {
   version = "2025.7",
   summary = "Content-addressed object storage and versioning (ostree)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/libostree/libostree-2025.7.tar.gz",
-  sha256  = "496ea5615a2758de82346d1cb2dd50c8e5ff9bf70bb1135ca56cb82254653335",
+  sha256  = "35dc91e377deb0d28dd09a160b55fb9b577afd641fa85a4b40b2efd6b2595af9",
   deps    = { "libfuse3", "libsoup3", "libcurl", "libarchive", "composefs", "avahi", "libxml2", "gpgme" },
   archive = { strip = 1 },
   test    = function(p)
