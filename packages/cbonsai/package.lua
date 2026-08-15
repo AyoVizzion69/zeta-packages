7af86aa4629b25897567c78bbf2d0305e338ea57
vizzion <AyoVizzion@protonmail.com>
2026-08-15 17:18:16 -0500

    Add package file for cbonsai

--- packages/cbonsai/package.lua
+++ packages/cbonsai/package.lua
@@ -1,1 +1,12 @@
-
+return {
+  name    = "cbonsai",
+  version = "master",
+  summary = "C Bonsai terminal screensaver",
+  url     = "https://raw.githubusercontent.com/AyoVizzion69/zeta-packages/refs/heads/main/packages/cbonsai/cbonsai-master.tar.gz",
+  sha256  = "c178decca03711e0d5e1f58a25c42a99b7d42e8d1b3798fb6423750b6f378149",
+  deps    = { "ncurses" },
+  archive = { strip = 1 },
+  test    = function(p)
+    p:run("test -f " .. p.install_root .. "/usr/bin/cbonsai")
+  end,
+}
